29b22cb8c9984bba209dae8f48631ba77eb0a39c
jihonlee <jihonlee@naver.com>
2017-04-03 22:51:56 +0900

    update!

--- javascript.js
+++ javascript.js
@@ -11,6 +11,42 @@ $(function() {
 	var chooseSpellId = new Array();
 	var chooseIdx = 0;
 
+			var mgCardName = new Array();	// 벖
+			var mgCardId = new Array();
+			var mgIdx = 0;
+
+			var wrCardName = new Array();	// 치킨
+			var wrCardId = new Array();
+			var wrIdx = 0;
+
+			var wlCardName = new Array();	// 흑마
+			var wlCardId = new Array();
+			var wlIdx = 0;
+
+			var shCardName = new Array();	// 쓰랄
+			var shCardId = new Array();
+			var shIdx = 0;
+
+			var prCardName = new Array();	// 사제
+			var prCardId = new Array();
+			var prIdx = 0;
+
+			var pdCardName = new Array();	// **사
+			var pdCardId = new Array();
+			var pdIdx = 0;
+
+			var htCardName = new Array();	// M.O
+			var htCardId = new Array();
+			var htIdx = 0;
+
+			var drCardName = new Array();	// 노루
+			var drCardId = new Array();
+			var drIdx = 0;
+
+			var rgCardName = new Array();	// ㅎㅂㅈ
+			var rgCardId = new Array();
+			var rgIdx = 0;
+
 	var select1Val;
 	var select2Val;
 
@@ -24,6 +60,10 @@ $(function() {
 	var rndIdx;
 
 	var rndTarget;
+
+	var selectedClass = -1;
+
+	var type = new Array(-1, -1, -1);
 
 	var isPlaying = false;
 
@@ -53,12 +93,71 @@ $(function() {
 						targetSpellId[targetIdx] = data[index].id;
 						targetIdx++;
 					}
+
+					switch(data[index].playerClass) {
+								case "MAGE":
+								mgCardName[mgIdx] = data[index].name;
+								mgCardId[mgIdx] = data[index].id;
+								mgIdx++;
+								break;
+
+								case "WARLOCK":
+								wlCardName[wlIdx] = data[index].name;
+								wlCardId[wlIdx] = data[index].id;
+								wlIdx++;
+								break;
+
+								case "SHAMAN":
+								shCardName[shIdx] = data[index].name;
+								shCardId[shIdx] = data[index].id;
+								shIdx++;
+								break;
+
+								case "WARRIOR":
+								wrCardName[wrIdx] = data[index].name;
+								wrCardId[wrIdx] = data[index].id;
+								wrIdx++;
+								break;
+
+								case "PRIEST":
+								prCardName[prIdx] = data[index].name;
+								prCardId[prIdx] = data[index].id;
+								prIdx++;
+								break;
+
+								case "PALADIN":
+								pdCardName[pdIdx] = data[index].name;
+								pdCardId[pdIdx] = data[index].id;
+								pdIdx++;
+								break;
+
+								case "HUNTER":
+								htCardName[htIdx] = data[index].name;
+								htCardId[htIdx] = data[index].id;
+								htIdx++;
+								break;
+
+								case "ROGUE":
+								rgCardName[rgIdx] = data[index].name;
+								rgCardId[rgIdx] = data[index].id;
+								rgIdx++;
+								break;
+
+								case "DRUID":
+								drCardName[drIdx] = data[index].name;
+								drCardId[drIdx] = data[index].id;
+								drIdx++;
+								break;
+					}
 				}
 			});
 		}
 	});
 
 	$.when(ajaxRequest).done(function() {
+		var classCardName = new Array(mgCardName, shCardName, wlCardName, prCardName, pdCardName, htCardName, wrCardName, drCardName, rgCardName);
+		var classCardId = new Array(mgCardId, shCardId, wlCardId, prCardId, pdCardId, htCardId, wrCardId, drCardId, rgCardId);
+		var classCardIdx = new Array(mgIdx, shIdx, wlIdx, prIdx, pdIdx, htIdx, wrIdx, drIdx, rgIdx);
 
 		$("#select1").change(function() {
 			var temp = 0;
@@ -82,69 +181,80 @@ $(function() {
 			}
 		});
 
+		$(".selectBtn").click(function() {
+			if(isPlaying == false) {
+				selectedClass = ($("p").index(this));
+
+				$(".selectBtn").css("background-color", "#fff");
+				$(".selectBtn").css("color", "#3668a7");
+
+				$(".selectBtn:nth-child(" + ($("p").index(this) + 1) + ")").css("background-color", "#3668a7");
+				$(".selectBtn:nth-child(" + ($("p").index(this) + 1) + ")").css("color", "#fff");
+			}
+		});
+
 		$("#gameBtn1").click(function() {
-			isPlaying = true;
+			if(selectedClass >= 0) {
+				isPlaying = true;
 
-			$("#cardDiv div").css("display", "inline-block");
-			$("label").css("display", "inline-block");
+				$("#cardDiv div").css("display", "inline-block");
+				$("label").css("display", "inline-block");
 
-			var select1Value = (Number)($("#select1").val());
-			var select2Value = (Number)($("#select2").val());
+				var select1Value = (Number)($("#select1").val());
+				var select2Value = (Number)($("#select2").val());
 
-			var isDuplicated = false;
-			var dIdx = 0;
-			var sum = simpleIdx + targetIdx + chooseIdx;
-			duplicatedName = new Array();
-			////alert(simpleIdx + "@" + targetIdx + "@" + chooseIdx);
-			for(var i=1; i<=3; i++) {
-				//////alert("#card" + i);
-				$("#card" + i).css("opacity", "100");
-				$("#label" + i).css("opacity", "100");
-				$("#field1 img, #field2 img").css("filter", "grayscale(100%)");
+				var isDuplicated = false;
+				var dIdx = 0;
 
-				rnd = Math.floor(Math.random() * sum) + 1;
-				//////alert(rnd);
+				type = new Array(-1, -1, -1);
 
-				for(var j in duplicatedName) {
-					if(duplicatedName[j] == rnd) {
-						isDuplicated = true;
+				duplicatedName = new Array();
+				////alert(simpleIdx + "@" + targetIdx + "@" + chooseIdx);
+				for(var i=1; i<=3; i++) {
+					//////alert("#card" + i);
+					$("#card" + i).css("opacity", "100");
+					$("#label" + i).css("opacity", "100");
+					$("#field1 img, #field2 img").css("filter", "grayscale(100%)");
+
+					rndIdx = Math.floor(Math.random() * classCardIdx[selectedClass]) + 1;
+					//////alert(rnd);
+
+					for(var j in duplicatedName) {
+						if(duplicatedName[j] == rndIdx) {
+							isDuplicated = true;
+						}
 					}
+
+					if(isDuplicated) {
+						i--;
+						isDuplicated = false;
+						continue;
+					}
+
+					duplicatedName[dIdx] = rndIdx;
+					dIdx++;
+
+					for(var j in simpleSpellId) {
+						if(simpleSpellId[j] == classCardId[selectedClass][rndIdx-1]) {
+							type[i-1] = 0;
+						}
+					}
+
+					if(type[i-1] == -1) {
+						for(var j in targetSpellId) {
+							if(targetSpellId[j] == classCardId[selectedClass][rndIdx-1]) {
+								type[i-1] = 1;
+							}
+						}
+					}
+
+					if(type[i-1] == -1) {
+						type[i-1] = 2;
+					}
+
+					$("#card" + i).css("background-image", "url(\"http://media.services.zam.com/v1/media/byName/hs/cards/enus/" + classCardId[selectedClass][rndIdx-1] + ".png\"");
+					$("#label" + i).text(classCardName[selectedClass][rndIdx-1]);
 				}
-
-				if(isDuplicated) {
-					i--;
-					isDuplicated = false;
-					continue;
-				}
-
-				duplicatedName[dIdx] = rnd;
-				dIdx++;
-
-				console.log(rnd);
-
-				if(rnd <= simpleIdx) {
-					rndIdx = rnd;
-					$("#card" + i).css("background-image", "url(\"http://media.services.zam.com/v1/media/byName/hs/cards/enus/" + simpleSpellId[rndIdx-1] + ".png\"");
-					$("#label" + i).text(simpleSpellName[rndIdx-1]);
-				} else if(rnd <= simpleIdx + targetIdx) {
-					rndIdx = rnd - simpleIdx;
-					$("#card" + i).css("background-image", "url(\"http://media.services.zam.com/v1/media/byName/hs/cards/enus/" + targetSpellId[rndIdx-1] + ".png\"");
-					$("#label" + i).text(targetSpellName[rndIdx-1]);
-
-					rndTarget = Math.floor(Math.random() * (select1Value + select2Value)) + 1;
-
-					console.log(rndTarget);
-				} else {
-					rndIdx = rnd - simpleIdx - targetIdx;
-					$("#card" + i).css("background-image", "url(\"http://media.services.zam.com/v1/media/byName/hs/cards/enus/" + chooseSpellId[rndIdx-1] + ".png\"");
-
-					$("#label" + i).text(chooseSpellName[rndIdx-1]);
-
-					rndTarget = Math.floor(Math.random() * (select1Value + select2Value)) + 1;
-
-					console.log(rndTarget);
-				}
-				////alert(duplicatedName);
 			}
 		});
 
@@ -155,7 +265,7 @@ $(function() {
 				$("#cardDiv div:not(:nth-child(" + (($("div").index(this)) - 4) + ")").css("display", "none");
 				$(".cardname:not(#label" + (($("div").index(this)) - 4) + ")").css("display", "none");
 
-				if((duplicatedName[(($("div").index(this)) - 5)]) > simpleIdx) {
+				if((type[(($("div").index(this)) - 5)]) > 0) {
 					console.log((duplicatedName[(($("div").index(this)) - 5)]) + "@");
 					var select1Value = (Number)($("#select1").val());
 					var select2Value = (Number)($("#select2").val());
